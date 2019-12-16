255c9c754dcbf21ea458900f87772e097ce8254e
Julien <julien.dev.paris@gmail.com>
2019-12-17 00:31:55 +0100

    exemple 2 with service account

--- index2.php
+++ index2.php
@@ -12,38 +12,28 @@ ini_set('display_errors', ($_ENV['ENVIRONEMENT'] === 'development') ? 1 : 0);
 include_once 'office365Interface.php';
 $clientOffice = new office365Interface();
 $accesToken = $clientOffice->getAccessTokenByCredential();
-$body = [
-  'enable'=> true,
-  'name'=> 'audrey',
-  'mailNickname'=> 'audrey',
-  'password'=> '~JceB]ggg9Ns@|>H',
-  'userType'=> 'Membre',
-  'firstname'=> 'audrey',
-  'lastname'=> 'audrey',
-  'mobilePhone'=> '0663341706',
-  'job'=> 'developpeur',
-  'otherMails' => ['audrey.deevd.paris@gmail.com'],
-  'userType' => 'Guest'
 
-];
-//$clientOffice->createOneUser($accesToken, $body);
-//$clientOffice->deleteOneUserById($accesToken, '839c5346-5ddf-4d19-90bf-8bcddbacc023');
-//$clientOffice->deleteOneUserByPrincipalName($accesToken,'upnvalue@testDevJulien.onmicrosoft.com ');
-//$users = $clientOffice->getInfoUsers($accesToken);
-//$photo = $clientOffice->getOneUsersPhoto($accesToken,'02fcd2a7-9781-4d4e-a716-e54f767aaf60');
 
-$dataContact = [
- 'assistantName' => 'juju',
-  'givenName' => 'julien',
-  'companyName' => 'janneautcf',
-  'displayName' => 'julienjanneau',
-  'email' => 'julisssssen@testDevJulien.onmicrosoft.com',
-  'id' => 'julien@free.fr'
-];
-$contact = $clientOffice->addContactUserById($accesToken,'02fcd2a7-9781-4d4e-a716-e54f767aaf60',$dataContact);
-//$contact = $clientOffice->addContactUserByUserPrincipalName($accesToken, 'audrey12@testDevJulien.onmicrosoft.com', $dataContact);
-var_dump($contact);
-/*foreach ($users as $user) {
+$organisations = $clientOffice->getOrganization($accesToken);?>
+<h1>Organisation</h1>
+<?php
+foreach($organisations as $organisation) {
+  echo $organisation->getDisplayName().'</br>';
+  echo '<pre>';
+  var_dump($organisation->getProperties());
+  echo '</pre>';
+  echo '<br>';
+  $verifiesDomains = $organisation->getVerifiedDomains();
+  foreach ($verifiesDomains as $domain) {
+    $domainOrganisationEmail = $domain['name'];
+  }
+}?>
+
+<h1>List des users </h1>
+<?
+$users = $clientOffice->getInfoUsers($accesToken); // ou $users = $clientOffice->getDeltaUsers($accesToken);
+foreach ($users as $user) {
+
   $businessPhone = (count($user->getBusinessPhones())> 0) ? $user->getBusinessPhones()[0] : '';
   echo 'name: ' . $user->getGivenName() . '<br>';
   echo 'display name: ' . $user->getDisplayName() . '<br>';
@@ -55,34 +45,45 @@ var_dump($contact);
   echo 'langue prefere: ' . $user->getPreferredLanguage() . '<br>';
   echo 'sur nom: ' . $user->getSurname() . '<br>';
   echo 'userPrincipalName: ' . $user->getUserPrincipalName() . '<br>';
-  echo 'id: ' . $user->getId() . '<br>';
-}*/
-/*$user = $clientOffice->getOneUserById($accesToken, '02fcd2a7-9781-4d4e-a716-e54f767aaf60');
-echo 'name: ' . $user->getGivenName() . '<br>';
-echo 'display name: ' . $user->getDisplayName() . '<br>';
-echo 'job: ' . $user->getJobTitle() . '<br>';
-echo 'mail: ' . $user->getMail() . '<br>';
-echo 'phone: ' . $user->getMobilePhone() . '<br>';
-echo 'location: ' . $user->getOfficeLocation() . '<br>';
-echo 'langue prefere: ' . $user->getPreferredLanguage() . '<br>';
-echo 'sur nom: ' . $user->getSurname() . '<br>';
-echo 'userPrincipalName: ' . $user->getUserPrincipalName() . '<br>';
-echo 'id: ' . $user->getId() . '<br>';
-$user = $clientOffice->getOneUserByPrincipalName($accesToken, 'audrey@testDevJulien.onmicrosoft.com');
-echo 'rrr';
-echo 'name: ' . $user->getGivenName() . '<br>';
-echo 'display name: ' . $user->getDisplayName() . '<br>';
-echo 'job: ' . $user->getJobTitle() . '<br>';
-echo 'mail: ' . $user->getMail() . '<br>';
-echo 'phone: ' . $user->getMobilePhone() . '<br>';
-echo 'location: ' . $user->getOfficeLocation() . '<br>';
-echo 'langue prefere: ' . $user->getPreferredLanguage() . '<br>';
-echo 'sur nom: ' . $user->getSurname() . '<br>';
-echo 'userPrincipalName: ' . $user->getUserPrincipalName() . '<br>';
-echo 'id: ' . $user->getId() . '<br>';*/
+  echo 'id: ' . $user->getId() . '<br><br>';
 
-//$clientOffice->updateOneUserById($accesToken, '02fcd2a7-9781-4d4e-a716-e54f767aaf60',['name'=>'audrey1']);
-//$clientOffice->updateOneUserByPrincipalName($accesToken, 'audrey@testDevJulien.onmicrosoft.com',['mailNickname'=>'audrey12'])
-//$users = $clientOffice->getDeltaUsers($accesToken);
+
+  //$clientOffice->updateOneUserById($accesToken, $user->getId(),['name'=>'zzzzz']);
+  //$clientOffice->updateOneUserByPrincipalName($accesToken, $user->getUserPrincipalName(),['mailNickname'=>'zzzzzz'])
+  //$clientOffice->deleteOneUserById($accesToken, $user->getId());
+  //$clientOffice->deleteOneUserByPrincipalName($accesToken, $user->getUserPrincipalName());
+
+  /*$dataAddContactUser = [
+    'assistantName' => 'zzz',
+    'givenName' => 'zzzz',
+    'companyName' => 'zzzz',
+    'displayName' => 'zzzz',
+    'email' => 'zzzzzz@testDevJulien.onmicrosoft.com'
+  ];*/
+  //$contact = $clientOffice->addContactUserById($accesToken,$user->getId(),$dataAddContactUser);
+  //$contact = $clientOffice->addContactUserByUserPrincipalName($accesToken, $user->getUserPrincipalName(), $dataAddContactUser);
+  //$user = $clientOffice->getOneUserById($accesToken, $user->getId());
+  //$clientOffice->getOneUserByPrincipalName($accesToken, $user->getUserPrincipalName());
+
+}
+
+
+// creation d'un utilisateur
+/*$bodyCreateUser = [
+  'enable'=> true,
+  'name'=> 'xxx',
+  'mailNickname'=> 'xxxx',
+  'password'=> '~JceB]sssfpejn@|>H',
+  'firstname'=> 'xxxx',
+  'lastname'=> 'xxx',
+  'mobilePhone'=> '0663342436',
+  'job'=> 'analyste',
+  'otherMails' => ['juju.pxxx@gmail.com'],
+  'userType' => 'Member'
+];
+$clientOffice->addOneUser($accesToken, $bodyCreateUser);*/
+
+
+//$photo = $clientOffice->getOneUsersPhoto($accesToken,'02fcd2a7-9781-4d4e-a716-e54f767aaf60');
 
 ?>
